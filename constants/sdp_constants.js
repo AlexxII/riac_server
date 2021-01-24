b06024a67e14f2d37b0ad892158bb47a3cf80896
AlexxII <murmus51@yandex.ru>
2021-01-24 21:11:43 +0300

    update

---
+++ constants/sdp_constants.js
@@ -0,0 +1,36 @@
+const delo = {
+  id: '',
+  hotKey: '',
+  fields: [
+    {
+      title: ''
+    }
+  ]
+}
+
+const user = {
+  title: 'У исполнителя',
+  hotKey: ''
+}
+
+const journal = {
+  title: 'По журналу',
+  hotKey: ''
+}
+
+const registry = {
+  title: 'По реестру',
+
+}
+
+const notAttached = {
+  title: 'Не приобщен'
+}
+
+module.exports = {
+  delo,
+  user,
+  journal,
+  registry,
+  notAttached
+}
